acf53217b0a3b6ff28c542dd63deb6553552561f
varunnegiweb@gmail.com <varunnegiweb@gamil.com>
2025-11-29 12:15:31 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/H3-uiuxpage/"
+  base: "/H3-uiuxpage/",
 })
